42a4717091b7608e0773cfae8828eecbf1551210
JDipi <48573618+JDipi@users.noreply.github.com>
2022-09-08 19:57:45 -0700

    added access to more domains
    added access to more domains, error handling, stability fixes

--- Smugmug Album Downloader.user.js
+++ Smugmug Album Downloader.user.js
@@ -10,21 +10,20 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @grant        GM_download
 // @grant        GM_addStyle
+// @grant        GM_info
 // ==/UserScript==
 
 /***************** IMPORTANT!!! ********************
 For this script to work properly, do the following:
-
   1) Go to your Tampermonkey dashboard
   2) Click settings
   3) Change the 1st option (Config Mode) to "Advanced"
   4) Scroll down to "Downloads BETA"
   5) Change "Download Mode" to "Browser API"
-  6) Save and exit, accept the new "Manage Download" 
+  6) Save and exit, accept the new "Manage Download"
      permission if prompted.
-
-Doing this allows Tampermonkey to save the images to a 
-folder in your default download location, rather than 
+Doing this allows Tampermonkey to save the images to a
+folder in your default download location, rather than
 just spewing all the images out.
 **************************************************/
 
@@ -34,49 +33,81 @@ $(document).ready(() => {
   );
 
   GM_addStyle(/*css*/ `
-    .sm-gallery-cover-title {
-      display: flex;
-      gap: 10px;
-      align-items: center;
-    }
+  .sm-gallery-cover-title {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+  #downloadAlb {
+    height: 40px;
+    border-radius: 10px;
+  }
+  #downloadAlb:hover {
+    background-color: gray;
+  }
 
-    #downloadAlb {
-      height: 40px;
-      border-radius: 10px;
-    }
+  .downloadProgressContainer progress {
+    width: 100%;
+  }
 
-    #downloadAlb:hover {
-      background-color: gray;
-    }
+  .downloadProgressContainer {
+    position: fixed;
+    z-index: 100;
+    background: #adadad;
+    width: fit-content;
+    color: black;
+    padding: 10px;
+    font-weight: bold;
+    width: fit-content;
+  }
 
-    #downloadProgress {
-      position: fixed;
-      z-index: 100;
-      background: #adadad;
-      width: fit-content;
-      color: black;
-      padding: 10px;
-      font-weight: bold;
-    }
-  `);
+  #downloadProgress {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #errors > p {
+    color: red;
+    border-top: 1px solid black
+  }
+
+  #errors a code {
+    color: blue;
+    text-decoration: underline;
+  }
+
+  #errors {
+    max-height: 270px;
+    overflow: auto;
+    width: 800px;
+  }
+`);
 
   $("#downloadAlb").on("click", function (e) {
     let url = $("link[rel=alternate][type*=rss+xml]").attr("href");
-    let regex = new RegExp(/\d*_[a-zA-Z\d]*/gm);
 
-    let albumId = url.match(regex)[0].split("_")[0];
-    let albumKey = url.match(regex)[0].split("_")[1];
+    let albumRegex = new RegExp(/\d*_[a-zA-Z\d]*/gm);
+    let domainRegex = new RegExp(/\/\/(.*)(?=\.smugmug\.com)/gm);
+
+    let albumId = url.match(albumRegex)[0].split("_")[0];
+    let albumKey = url.match(albumRegex)[0].split("_")[1];
+    let domain = window.location.href.match(domainRegex);
     let links = [];
 
     const settings = {
       async: true,
       crossDomain: true,
-      url: `https://creanlutheran.smugmug.com/services/api/json/1.4.0?galleryType=album&albumId=${albumId}&albumKey=${albumKey}&PageNumber=2&imageId=0&returnModelList=true&PageSize=5000&method=rpc.gallery.getalbum`,
+      url: `https:${domain}.smugmug.com/services/api/json/1.4.0?galleryType=album&albumId=${albumId}&albumKey=${albumKey}&PageNumber=2&imageId=0&returnModelList=true&PageSize=5000&method=rpc.gallery.getalbum`,
       method: "GET",
     };
 
     $.ajax(settings).done(function (response) {
       console.log(response);
+
+      if (GM_info.downloadMode !== "browser") {
+        alert(`Please change the Tampermonkey download mode to browser and read the important comment at the top of the script!!!`)
+        return
+      }
 
       if (
         !confirm(
@@ -101,31 +132,75 @@ $(document).ready(() => {
           }
         }
 
-        let url = `https://photos.smugmug.com/photos/${picID}/0/${largestSize}/${picID}-${largestSize}.${ext}`;
+        let url = `https://photos.smugmug.com/photos/${picID}/0/${largestSize}/${picID}-${largestSize}.${ext}s`;
         links.push(url);
       }
 
-      $("body").prepend(
-        `<div id="downloadProgress">Downloading: [/${response.Pagination.TotalItems}]</div>`
-      );
       let done = 0;
+
+      
+      $("body").prepend(
+        `<div class="downloadProgressContainer">
+          <div id="downloadProgress">
+            Downloading: [/${response.Pagination.TotalItems}]
+          </div>
+            <progress id="dp" value="" max="${response.Pagination.TotalItems}"></progress>
+            <div id="errors"></div>
+            <button id="close">Close</button>
+        </div>`
+      );
+
+      $('#close').on('click', function(e) {
+        links = []
+        done = 0
+        $('.downloadProgressContainer').remove()
+      })
+
+      // checks if the whole list has been run through and displays error / finished message
+      const finish = () => {
+        if (done == links.length) {
+          $("#downloadProgress").text("Finished!");
+          if ($('#errors').children().length) {
+            $("#downloadProgress").text(`Finished with ${$('#errors').children().length} errors.`);
+          }
+        }
+      }
+
       links.forEach((url, i) => {
         let name = response.Albums[0].Title + "/" + url.split("/").slice(-1)[0];
         GM_download({
           url,
           name,
-          onload: function (e) {
+          onload: (e) => {
             done += 1;
             $("#downloadProgress").text(
               `Downloading: [${done}/${response.Pagination.TotalItems}]`
             );
-            if (done == links.length) {
-              $("#downloadProgress").text("Finished!");
-              setTimeout(() => {
-                $("#downloadProgress").remove();
-              }, 3000);
-            }
+            $("progress#dp").attr("value", done)
+            finish()
           },
+          onerror: (e) => {
+            done += 1
+            $("#errors").append(
+              `<p>Error: <code>${e.error}</code> for image [${i+1}/${response.Pagination.TotalItems}] 
+                <a href="${url}" target="_blank">
+                  <code>${name}</code>
+                </a>
+              </p>`
+              );
+              finish()
+          },
+          ontimeout: (e) => {
+            done += 1
+            $('#errors').append(
+              `<p>The request for image [${i+1}/${response.Pagination.TotalItems}]
+                <a href="${url}" target="_blank">
+                  <code>${name}</code>
+                </a>
+              Timed out</p>`
+            )
+            finish()
+          }
         });
       });
     });
